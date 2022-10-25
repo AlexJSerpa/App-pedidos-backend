6b9ee26cf493563c4fc7688042f15322b467e399
Alex J Serpa <109832670+Azrael31alex@users.noreply.github.com>
2022-10-25 16:36:14 -0500

    Update mongodb.datasource.ts

--- src/datasources/mongodb.datasource.ts
+++ src/datasources/mongodb.datasource.ts
@@ -4,7 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'mongodb',
   connector: 'mongodb',
-  url: 'mongodb+srv://Prog_web_Azrael:ProWebMintic2022@clusterproweb.hddnrwo.mongodb.net/PedidosBD?retryWrites=true&w=majority',
+  url: 'mongodb+srv:',
   host: '',
   port: 0,
   user: '',
